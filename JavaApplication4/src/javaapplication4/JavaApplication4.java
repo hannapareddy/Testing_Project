740c321af8948fa72c3eb26f9a2f859f6effb4dd
harish <reddy.smile16@gmail.com>
2017-06-27 14:39:14 -0400

    US12345:added the configuration java application.

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Change the name ");
+        System.out.println("Change the name Harish ");
         // TODO code application logic here
     }
     
