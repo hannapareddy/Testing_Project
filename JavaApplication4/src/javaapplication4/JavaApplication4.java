c0bd107971475ce3bd6746e29f849cc83e71b368
harish <reddy.smile16@gmail.com>
2017-06-27 14:37:44 -0400

    US1234:added the configuration java application.

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Change the name ");
         // TODO code application logic here
     }
     
